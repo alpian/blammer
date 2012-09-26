8d08293a4744dd4bbf959ba8b2dc3cfd602d7b48
Ian Davies <ian.davies@timgroup.com>
2012-09-26 13:05:12 +0200

    Adding clj-http dependency

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Blammer: analysing the chatter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-http "0.5.5"]])
